1a839fd7f7154be6c2ea8cbe136446bea8223f00
hkeper <hkeper1@gmail.com>
2020-05-04 00:33:22 +0300

    Add solution for the course task 3.3 - Robot

---
+++ 33/src/Main.java
@@ -0,0 +1,87 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Robot robot = new Robot(0,0, Direction.DOWN);
+        moveRobot(robot, -10, 20);
+
+    }
+
+    public enum Direction {
+        UP,
+        DOWN,
+        LEFT,
+        RIGHT
+    }
+
+    public static class Robot {
+        int x;
+        int y;
+        Direction dir;
+
+        public Robot (int x, int y, Direction dir) {
+            this.x = x;
+            this.y = y;
+            this.dir = dir;
+        }
+
+        public Direction getDirection() {return dir;}
+
+        public int getX() {return x;}
+
+        public int getY() {return y;}
+
+        public void turnLeft() {
+            if      (dir == Direction.UP)    {dir = Direction.LEFT;}
+            else if (dir == Direction.DOWN)  {dir = Direction.RIGHT;}
+            else if (dir == Direction.LEFT)  {dir = Direction.DOWN;}
+            else if (dir == Direction.RIGHT) {dir = Direction.UP;}
+        }
+
+        public void turnRight() {
+            if      (dir == Direction.UP)    {dir = Direction.RIGHT;}
+            else if (dir == Direction.DOWN)  {dir = Direction.LEFT;}
+            else if (dir == Direction.LEFT)  {dir = Direction.UP;}
+            else if (dir == Direction.RIGHT) {dir = Direction.DOWN;}
+        }
+
+        public void stepForward() {
+            if (dir == Direction.UP)    {y++;}
+            if (dir == Direction.DOWN)  {y--;}
+            if (dir == Direction.LEFT)  {x--;}
+            if (dir == Direction.RIGHT) {x++;}
+        }
+    }
+
+    public static void moveRobot(Robot robot, int toX, int toY) {
+        Direction trueDirectionX = Direction.LEFT;
+        Direction trueDirectionY = Direction.UP;
+        int xi = robot.getX();
+        int yi = robot.getY();
+
+        if ((toX - xi)>0){
+            trueDirectionX = Direction.RIGHT;
+        }
+
+        chooseDirection(trueDirectionX, robot);
+
+        while (robot.getX() != toX){
+            robot.stepForward();
+        }
+
+        if ((toY - yi)<0){
+            trueDirectionY = Direction.DOWN;
+        }
+        chooseDirection(trueDirectionY, robot);
+
+        while (robot.getY() != toY){
+            robot.stepForward();
+        }
+    }
+
+    private static void chooseDirection(Direction dir, Robot robot){
+        while (robot.getDirection() != dir) {
+            robot.turnLeft();
+        }
+    }
+
+}
